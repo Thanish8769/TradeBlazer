593089e61fb585ad5c6e0c57137ca77926bd7736
Thanish8769 <59763181+Thanish8769@users.noreply.github.com>
2024-03-06 23:22:08 +0000

    Update main.py

--- OptionsProject/main.py
+++ OptionsProject/main.py
@@ -1,5 +1,4 @@
-import streamlit as st
-from streamlit_modal import Modal   
+import streamlit as st  
 from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 import pandas as pd
